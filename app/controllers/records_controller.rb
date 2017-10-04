f89978dd22ae5c7da6cdc385e164c738e180a43e
Iscenco Alexandr <iscencoal@gmail.com>
2017-10-04 13:29:04 +0000

    final commit

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,4 +4,36 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    head :no_content
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def record_params
+      params.require(:record).permit(:title, :amount, :date)
+    end
+
 end
